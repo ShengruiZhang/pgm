d0c8a0a7b0ed758ed71139a33996448a86990370
Ray <msi1459481096@gmail.com>
2018-11-29 01:52:57 -0700

    NFG Proj5 update

--- proj5/src/main.cpp
+++ proj5/src/main.cpp
@@ -14,48 +14,74 @@
 #include <unordered_map>
 #include <algorithm> // To use std::find
 
-#define GLOBAL_NO_DEBUG 1
+#define GLOBAL_NO_DEBUG 0
 
 #if GLOBAL_NO_DEBUG
+#define GLOBAL_DEBUG 0
 #define DEBUG_FILE_INPUTING 0
+#define DEBUG_BFS 0
 #else
-#define DEBUG_FILE_INPUTING 1
+
+
+// Toggle each main debugging category
+#define GLOBAL_DEBUG 0
+#define DEBUG_FILE_INPUTING 0
+#define DEBUG_BFS 1
 #endif
 
 
-#if !DEBUG_FILE_INPUTING
-#define DEBUG_INPUT 0
-#define DEBUG_GETLINE 0
-#define DEBUG_INPUT_COUNT 0
-#define DEBUG_SEARCH_MATCHING 0
-#define DEBUG_PRINT_LIST 0
-#define DEBUG_NODE 0
-#else
+#if DEBUG_FILE_INPUTING
+// Toggle each sub-category debugging
 #define DEBUG_INPUT 1
 #define DEBUG_GETLINE 1
 #define DEBUG_INPUT_COUNT 1
 #define DEBUG_SEARCH_MATCHING 1
 #define DEBUG_PRINT_LIST 0
 #define DEBUG_NODE 1
+
+
+#else
+#define DEBUG_INPUT 0
+#define DEBUG_GETLINE 0
+#define DEBUG_INPUT_COUNT 0
+#define DEBUG_SEARCH_MATCHING 0
+#define DEBUG_PRINT_LIST 0
+#define DEBUG_NODE 0
 #endif
 
 
+#if DEBUG_BFS
+// Toggle each sub-category debugging
+#define DEBUG_BFS_QUEUE 1
+#define DEBUG_BFS_DICV_SET 1
+#define DEBUG_BFS_ADJ 1
+
+
+#else
+#define DEBUG_BFS_QUEUE 0
+#define DEBUG_BFS_DICV_SET 0
+#define DEBUG_BFS_ADJ 0
+#endif
 
 typedef unsigned int uint;
 
 using namespace std;
 
 /*--------------------------------------------------------------------------*/
-uint ReadingFile( unordered_map<string, Edge> &, vector<Node> &, string);
-void PrintingList( unordered_map<string, Edge> &);
+uint ReadingFile		( unordered_map<string, Edge> &, vector<Node> &, string);
+void PrintingList		( unordered_map<string, Edge> &);
+Node SearchNode			( vector<Node>, string);
 /*--------------------------------------------------------------------------*/
 
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//////////////// Begin of Main function /////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
 int main(int argc, char *argv[]) {
 	if(argc != 4)
 	{
-#if !GLOBAL_NO_DEBUG
+#if GLOBAL_DEBUG
 		cout << "Don't have enough arguments." << endl;
 #endif
 		return 0;
@@ -69,24 +95,12 @@ int main(int argc, char *argv[]) {
 	/*----------------------------------------------*/
 	unordered_map <string, Edge> MainList;
 	vector<Node> MainNode;
-#if !GLOBAL_NO_DEBUG
+#if GLOBAL_DEBUG
 	cout << "Unordered map created." << endl;
 #endif
 
 	// Read the input file
 	ReadingFile(MainList, MainNode, inputFileName);
-
-	// Calculate the count of followers
-	for(uint i = 0; i < MainNode.size(); ++i)
-	{
-		MainNode.at(i).CNT_Follower = MainNode.at(i).IncomingEdge.size();
-#if DEBUG_NODE
-		cout << "Username: "
-			<< MainNode.at(i).Username
-			<< " has " << MainNode.at(i).CNT_Follower
-			<< " followers." << endl;
-#endif
-	}
 
 #if DEBUG_PRINT_LIST
 	PrintingList(MainList);
@@ -96,8 +110,94 @@ int main(int argc, char *argv[]) {
 	// Nodes to be visited will be added to the queue
 	// Use push_back to add nodes to the end of the vector
 	// Use vector.erase(vector.begin()) to pop the first element
+	//
+
+	// Here starts the BFS search
+	
+	// We need a queue for upcoming nodes,
+	// 	a vector for discovered set
+	// 	maybe a another vector for storing the qulified items
+	
+	// The frontierQueue
+	vector<Edge> MainQueue;
+
+	// The discoveredSet
+	vector<Node> DiscoveredList;
+
+	// The qualified list, unsorted
+	vector<Node> QualifiedList;
+
+	Node currentNode;
+	Edge currentEdge;
 	
 
+	// Start the BFS from the user specified
+	// Push the starting node to MainQueue
+	MainQueue.push_back( MainList.at(Username) );
+
+	// Add the first node to DiscoveredList
+	for(uint i = 0; i < MainNode.size(); ++i)
+	{
+			if(MainNode.at(i).Username == Username)
+			{
+					DiscoveredList.push_back( MainNode.at(i) );
+					
+					// also set the currentNode for the first use
+					currentNode = MainNode.at(i);
+			}
+	}
+
+#if DEBUG_BFS_DICV_SET
+	cout << endl << "DiscoveredList Last: "
+			<< DiscoveredList.back().Username << endl;
+#endif
+
+#if DEBUG_BFS_QUEUE
+	cout << endl;
+	cout << "Debug: start at: " << Username
+			<< ". This Username follows these people: " << endl;
+	cout << endl;
+	for(uint i = 0; i < MainQueue.back().UserFollowing.size(); ++i)
+	{
+			cout << MainQueue.back().UserFollowing.at(i);
+	}
+	cout << endl;
+#endif
+
+	//------------------------------------------------------------------------------//
+	//------------------------------------------------------------------------------//
+	//	Main while loop for MainQueue
+	while( !MainQueue.empty() )
+	{
+			// Set current edge and current node
+			currentEdge = MainQueue.at(0);
+
+			// TODO: How to detemine the current node
+#if DEBUG_BFS_QUEUE
+			cout << endl;
+			cout << "----------------------------------------";
+			cout << endl << "Current Edge has these following: " << endl;
+			for(uint i = 0; i < currentEdge.UserFollowing.size(); ++i)
+			{
+							cout << currentEdge.UserFollowing.at(i) << endl;
+			}
+			cout << "Finished printing out current Edge." << endl;
+#endif
+
+			// Pop the first element of the queue
+			MainQueue.erase(MainQueue.begin());
+
+			// Visiting actions
+			// TODO
+
+
+			// for loop for adjacent Node
+			// TODO
+
+
+	}
+	//------------------------------------------------------------------------------//
+	//------------------------------------------------------------------------------//
 
 
 
@@ -112,11 +212,18 @@ int main(int argc, char *argv[]) {
 	oup_stream.close();
 	return 0;
 }
+////////////////// End of Function //////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 
 
-uint ReadingFile( unordered_map<string, Edge> &MainList,
-		vector<Node> &MainNode, string inputFileName)
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//////////////// Begin of Function //////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+uint ReadingFile(	unordered_map<string, Edge> &MainList,
+			vector<Node> &MainNode,
+			string inputFileName)
 {
 #if DEBUG_INPUT_COUNT
 	uint _counter = 0;
@@ -129,7 +236,6 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 #endif
 
 	inp_stream.open(inputFileName);
-
 	if( inp_stream.is_open() )
 	{
 #if DEBUG_INPUT
@@ -144,6 +250,9 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 		return 0;
 	}
 
+
+	/////////////////////////////////////////
+	// Create temps
 	string input_raw;
 	string Username_temp, UserFollowing_temp;
 	Edge temp_Edge;
@@ -152,6 +261,8 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 
 	uint node_flag = 0;
 	uint node_i = 0;
+	/////////////////////////////////////////
+
 
 #if DEBUG_INPUT
 	cout << "Input string steam created." << endl;
@@ -167,12 +278,14 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 		UserFollowing_temp.clear();
 		inp_SS.clear();
 		temp_Edge.UserFollowing.clear();
+		temp_Edge.depth = 0;
 
 #if DEBUG_GETLINE
 		cout << "-------------------------------------------" << endl;
 		cout << "Getting a line." << endl << endl;
 #endif
 
+		// Take a line from the file
 		getline(inp_stream, input_raw);
 
 		// Check for new line \n
@@ -181,14 +294,25 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 #if DEBUG_GETLINE
 			cout << "File bottom out." << endl;
 #endif
+		// Calculate the count of followers
+			for(uint i = 0; i < MainNode.size(); ++i)
+			{
+				MainNode.at(i).CNT_Follower = MainNode.at(i).IncomingEdge.size();
+#if DEBUG_NODE
+				cout << "Username: "
+					<< MainNode.at(i).Username
+					<< " has " << MainNode.at(i).CNT_Follower
+					<< " followers." << endl;
+#endif
+			}
 			return 1;
 		}
 
 #if DEBUG_GETLINE
 		cout << "Raw input: " << input_raw << endl << endl;
 #endif
+		// Stringstream operation
 		inp_SS.str(input_raw);
-
 		inp_SS >> UserFollowing_temp;
 		inp_SS >> Username_temp;
 
@@ -201,17 +325,19 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 #if DEBUG_INPUT_COUNT
 		++_counter;
 #endif
-		temp_Edge.depth = 0;
 
-		///////////////////////////////////////////////
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
 		//	MainList
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		// Making the graph by checking if the Username is already existed
 		search_match = MainList.find(Username_temp);
 		if( search_match == MainList.end() )
 		{// The Username is new, so create a new entry in the MainList
 
 #if DEBUG_SEARCH_MATCHING
-			cout << "No existing entry for Username: " << Username_temp
+			cout << "No existing entry for Username: "
+				<< Username_temp
 				<< endl;
 			cout << "Creating a new entry in unordered map." << endl;
 #endif
@@ -253,9 +379,12 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 #endif
 		}	// Finished the adding entry
 
-		//////////////////////////////////////////////////////
+
+
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
 		//	MainNode
-		//
+		/////////////////////////////////////////////////////////////////////////////////////////////////////
+
 		// Clear temp
 		temp_Node.Username.clear();
 		temp_Node.IncomingEdge.clear();
@@ -283,7 +412,6 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 				node_i = 999;
 			}
 		}
-
 
 #if DEBUG_NODE
 		cout << endl << "Node operation." << endl;
@@ -323,17 +451,20 @@ uint ReadingFile( unordered_map<string, Edge> &MainList,
 				<< MainNode.at(node_i).IncomingEdge.
 				back() << endl;
 #endif
-
 		}
-
-	}
+	}	// End while loop
 	inp_stream.close();
-
 	return 1;
 }
+//////////////// End of Function ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//////////////// Begin of Function //////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
 #if DEBUG_PRINT_LIST
 void PrintingList( unordered_map<string, Edge> &MainList)
 {
@@ -356,3 +487,30 @@ void PrintingList( unordered_map<string, Edge> &MainList)
 	cout << "Map size: " << MainList.size() << endl;
 }
 #endif
+//////////////// End of Function ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//////////////// Begin of Function //////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+Node SearchNode			( vector<Node> MainNode, string Username)
+{
+				uint index = 0;
+				for(index = 0; index < MainNode.size(); ++index)
+				{
+								if(MainNode.at(index).Username == Username)
+								{
+												return MainNode.at(index);
+								}
+				}
+
+				Node empty;
+				empty.Username = "Username cannot be found";
+				return empty;
+}
+//////////////// End of Function ////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
